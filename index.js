8d6d9608fa1e7deb8fab3415cc2e0ce634ca0196
Jason Walton <jwalton512@gmail.com>
2017-10-16 08:35:31 -0700

    remove comments

--- index.js
+++ index.js
@@ -33,38 +33,4 @@ module.exports = postcss.plugin('postcss-apply-class', function () {
             return rule.toString().replace(/\s/g, '').split(',');
         }
     };
-    // replace @apply declaration with matching root rule
-    /*
-    return function (root) {
-        // look through each root rule
-        root.walkRules(rule => {
-            // loop through @apply declarations
-            rule.walkAtRules('apply', apply => {
-                // loop through all root rules
-                root.walkRules(rootRule => {
-                    const applyClasses = utils.stringToList(apply.params);
-                    // for each @apply class, compare current root rule for a match
-                    applyClasses.forEach(applyClass => {
-                        // look at each selector in root rule i.e .foo, .bar
-                        rootRule.selectors.forEach(selector => {
-                            // determine if current selector matches @apply rule
-                            if (selector === applyClass) {
-                                // if we have a match, then apply all the declarations from the root rule
-                                rootRule.walkDecls(decl => {
-                                    rule.append({
-                                        prop: decl.prop,
-                                        value: decl.value
-                                    });
-                                });
-                            }
-                        });
-                    });
-                });
-
-                // remove the @apply rule
-                apply.remove();
-            });
-        });
-    };
-    */
 });
